f404b34caf8a0e6e5433c243f0b75c1a1fea7855
Christoph Hennemann <christoph.hennemann@gmail.com>
2016-02-21 00:19:47 +0100

    docs: Add javadoc to existing classes
    JavaDoc added to:
     - ReciprApplication
     - ApplicationComponent
     - ApplicationModule
    
    Also updated the xml style of the AndroidManifest.xml for better
    readability.
    
    Resolves: #10

--- app/src/main/java/de/androidbytes/recipr/app/di/components/ApplicationComponent.java
+++ app/src/main/java/de/androidbytes/recipr/app/di/components/ApplicationComponent.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016 Christoph Hennemann
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.androidbytes.recipr.app.di.components;
 
 import dagger.Component;
@@ -6,7 +21,7 @@ import de.androidbytes.recipr.app.di.modules.ApplicationModule;
 import javax.inject.Singleton;
 
 /**
- * Created by Christoph on 20.02.2016.
+ * Dagger Component used to expose application wide dependencies.
  */
 @Component(
         modules = ApplicationModule.class
--- app/src/main/java/de/androidbytes/recipr/app/di/modules/ApplicationModule.java
+++ app/src/main/java/de/androidbytes/recipr/app/di/modules/ApplicationModule.java
@@ -1,18 +1,54 @@
+/**
+ * Copyright (C) 2016 Christoph Hennemann
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.androidbytes.recipr.app.di.modules;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import dagger.Module;
-import de.androidbytes.recipr.app.Recipr;
+import dagger.Provides;
+import de.androidbytes.recipr.app.ReciprApplication;
+
+import javax.inject.Singleton;
 
 /**
- * Created by Christoph on 20.02.2016.
+ * Dagger Module for application wide dependencies
  */
 @Module
 public class ApplicationModule {
 
-    private Recipr mRecipr;
+    private ReciprApplication mReciprApplication;
 
-    public ApplicationModule(Recipr recipr) {
-        mRecipr = recipr;
+    /**
+     * Constructor of {@link ApplicationModule}.
+     * @param reciprApplication {@link ReciprApplication} instance referenced inside the
+     * {@link ApplicationModule}.
+     */
+    public ApplicationModule(ReciprApplication reciprApplication) {
+        mReciprApplication = reciprApplication;
+    }
+
+    /**
+     * Provider method to retrieve the applications default {@link SharedPreferences}.
+     *
+     * @return A {@link SharedPreferences} singleton used all over the application.
+     */
+    @Provides
+    @Singleton
+    public SharedPreferences provideSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(mReciprApplication);
     }
 
 }
